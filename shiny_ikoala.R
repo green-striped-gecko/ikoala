3d88cef798f4ef7d4b2861fb00ef55a6876ed04a
Bernd Gruber <bernd.gruber@canberra.edu.au>
2019-11-05 08:34:34 +0000

    corrected error in date new shiny now with monitoring/general

--- shiny_ikoala.R
+++ shiny_ikoala.R
@@ -2,22 +2,42 @@ library(DT)
 library(rhandsontable)
 library(shiny)
 library(shinyWidgets)
+library(shinyjs)
 shinyApp(
   ui = fluidPage(
+    useShinyjs(),  # Set up shinyjs
     setBackgroundImage(
       src = "./ikoala_report_title2.png"
     ),
     textInput(inputId = "report_name", "Report name:", "ikoala report"),
     selectizeInput(inputId = "database", label="Database: ", choices=list.files("database/", pattern = "accdb")),
-    selectizeInput(inputId = "selperiod", label="Periods: ", choices=list.files("periods/", pattern = "csv")),
-    rHandsontableOutput('table'),
+    selectizeInput(inputId = "selrestricted", label="Type of report:", choices=c("monitoring","general")),
+    
     selectizeInput(inputId = "selsites", label="Sites: ", choices=list.files("sites/", pattern = "csv")),
-    selectizeInput(inputId = "selrestricted", label="Restrict plots by sites or region: ", choices=c("site","region")),
+    selectizeInput(inputId = "selstudyarea", label="Study area:", choices=c("Coastal","Southern Tablelands","Palerang", "Kooraban/Gulaga","Yurammie","Escarpment/Hinterlands", "Tantawangalo","Escarpment/Hinterland","Strzelecki Ranges", "Bermagui","Devils Holes","Southern tablelands","Hinterland")),
+    h5(strong('Periods')),
+    p('for "general" exactly ',strong('one'),' period'), 
+    p('for monitoring at least ',strong('two'),' periods.'),
+    rHandsontableOutput('table'),
+    selectizeInput(inputId = "selperiod", label="Periods: ", choices=list.files("periods/", pattern = "csv")),
     
     textInput(inputId ="projection", "Projection: ","+proj=utm +zone=55 +south +ellps=WGS84 +datum=WGS84 +units=m +no_defs"),
     downloadButton("report", "Generate ikoala report", icon="images/ikoala.ico")
   ),
   server = function(input, output) {
+    
+    
+    observe({
+      #toggleState(id="selstudyarea", condition= input$selrestricted=="monitoring")
+      if (input$selrestricted=="monitoring") {disable("selstudyarea"); enable("selsites")}
+      if (input$selrestricted=="general") 
+      {enable("selstudyarea")
+        disable("selsites")
+        DF = hot_to_r(input$table)
+        output$table <- renderRHandsontable({rhandsontable(DF[1,])})}
+      })
+    
+    
     observeEvent(input$selperiod, {
       output$table <- renderRHandsontable(rhandsontable(read.csv(file.path("periods",input$selperiod)))) 
       })
@@ -26,11 +46,14 @@ shinyApp(
       output$table <- renderRHandsontable({
       if (is.null(input$table)) {
         DF = read.csv(file.path("periods",input$selperiod))
+        
       } else {
         
         DF = hot_to_r(input$table)
+       
       }
-      rhandsontable(DF) 
+       rhandsontable(DF)
+  
     })
   
     output$report <- downloadHandler(
@@ -39,17 +62,19 @@ shinyApp(
       content = function(file) {
         params <- list(report_name= input$report_name,
                        database= file.path("database",input$database),
-                       study_periods_name=input$selperiod,
-                       study_periods=hot_to_r(input$table),
+                       study_periods= file.path("periods","periods.csv"),
                        study_site= file.path("sites",input$selsites),
-                       restricted= input$selrestricted,
+                       report_type= input$selrestricted,
+                       study_area=  input$selstudyarea,
                        results_folder= "results",
-                       max_distance= (if (input$selrestricted=="site")   250 else 501),
                        projection= input$projection)
         
         # Knit the document, passing in the `params` list, and eval it in a
         # child of the global environment (this isolates the code in the document
         # from the code in this app).
+        #print(params)
+        #write periods to tempfile (only one string can be passed to YAML)
+        write.csv(hot_to_r(input$table), "./periods/periods.csv", row.names = F)
         rmarkdown::render("ikoala_monitoring_server.rmd", output_file = file,params=params,
                           envir = new.env(parent = globalenv())
         )
